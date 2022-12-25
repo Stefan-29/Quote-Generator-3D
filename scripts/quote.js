cc274f344ec2bbf8e84dc8043064073959c9d7b8
Stefan-29 <58769705+Stefan-29@users.noreply.github.com>
2022-12-25 13:44:02 +0700

    resizing quote text placeholder and adding particle effects

--- scripts/quote.js
+++ scripts/quote.js
@@ -2,10 +2,9 @@ const scene = new THREE.Scene();
 const cam = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
 
 // INIT CAMERA
-cam.position.z = 20;
+cam.position.z = 50;
 cam.position.x = 0;
-cam.position.y = 10;
-//cam.lookAt(-10, -20, -50)
+cam.position.y = 0;
 
 //INIT HEMISPHERE LIGHT
 scene.add(new THREE.AmbientLight(0xffffff, 0.5))
@@ -36,7 +35,33 @@ light1.shadow.mapSize.height = 4096;
 scene.add(light1)
 
 //BACKGROUND COLOR
-scene.background = new THREE.Color("rgb(0, 205, 254)")
+scene.background = new THREE.Color("rgb(254, 0, 254)")
+
+//PARTICLES
+const particlesGeometry = new THREE.BufferGeometry;
+const particlesCount = 5000
+
+const positionArr = new Float32Array(particlesCount * 3)
+
+// Add some random particle positions
+for (let i = 0; i < particlesCount * 3; i++) {
+    positionArr[i] = (Math.random() - 0.5) * 50 //centerilization
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArr, 3))
+    // Set up the particle material
+const particlesMaterial = new THREE.PointsMaterial({
+    color: 0xffe1fa,
+    size: 0.1,
+});
+
+// Create the particle system
+const particleSystem = new THREE.Points(particlesGeometry, particlesMaterial);
+
+
+//adding the particles to the scene
+scene.add(particleSystem)
+
 
 //TEXT LOADING
 const l = new THREE.FontLoader()
@@ -64,7 +89,7 @@ l.load("../fonts/Dark Magic_Regular.json", function(font) {
             var r = jsonObject.quotes[randomizer]
             if (q === r) {
                 const message = r.quote + "\n" + r.author;
-                const shapes = font.generateShapes(message, 5);
+                const shapes = font.generateShapes(message, 2);
 
                 const geometry = new THREE.ShapeGeometry(shapes);
 
@@ -72,28 +97,37 @@ l.load("../fonts/Dark Magic_Regular.json", function(font) {
 
                 const xMid = -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
 
-                geometry.translate(xMid, xMid, xMid);
-
-                // make shape ( N.B. edge view not visible )
+                geometry.translate(xMid, 0, xMid);
+                console.log(xMid)
+                cam.lookAt(xMid, 0, -70)
+                cam.fov = 5
+                    // make shape ( N.B. edge view not visible )
 
                 const text = new THREE.Mesh(geometry, matDark);
                 const text1 = new THREE.Mesh(geometry, matLite)
-                text.position.z = -50;
-                text1.position.z = -100
-                scene.add(text1)
-                scene.add(text);
-
-
+                text1.position.z = text.position.z - 0.1;
+                text1.position.y = text.position.y + 0.1;
+                text1.position.x = text.position.x - 0.1;
+                cam.position.x = text.position.x
+                cam.position.y = text.position.y
+                cam.position.z = text.position.z + 30
+                scene.add(text, text1)
             }
         }
     })
 
 })
 
+
+
 //ANIMATE
 function draw() {
     renderer.render(scene, cam)
+    particleSystem.rotation.y += .0005
+    particleSystem.rotation.x += .0005
+    particleSystem.rotation.z += .0005
+
     controls.update()
     requestAnimationFrame(draw)
 }
-draw();
+draw()
